95a2afad3746c41ba967ae0e94d93eb17f292ada
Ivars67 <ivars.kristapsons2008@gmail.com>
2025-12-10 08:43:19 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    
+    int n;
+    
+    cout<<"Ievadi skaitli reizrēķinam: "<<endl;
+    cin>>n;
+    cout<<endl<<endl;
+    
+    vector<vector<int>> reiz (n, vector<int> (n));
+    
+    //aizpilda tabulu
+    for(int i=0; i<n; i++){
+        
+        for(int j=0; j<n; j++){
+            
+            reiz[i][j] = (i+1) * (j+1);
+            
+        }
+    }
+    
+    //izprintē tabulu
+    for(int i=0; i<n; i++){
+        
+        for(int j=0; j<n;j++){
+            
+            cout<<reiz[i][j]<<" ";
+            
+        }
+        
+        cout<<endl;
+        
+    }
+    
+    return 0;
+    
+}
